1bae212aaadea054909d1136e085865e480b46cb
Sam <samg05981@gmail.com>
2022-10-02 01:23:37 +0400

    not completed

--- app/src/main/java/com/example/tic_tac_toe/HomeActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/HomeActivity.kt
@@ -3,19 +3,24 @@ package com.example.tic_tac_toe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 import androidx.appcompat.widget.AppCompatButton
 
 class HomeActivity : AppCompatActivity() {
 
-    lateinit var button_play: AppCompatButton
-    lateinit var button_quit: AppCompatButton
-
+    private lateinit var button_play: AppCompatButton
+    private lateinit var button_quit: AppCompatButton
+private lateinit var button_help:AppCompatButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
         button_quit = findViewById(R.id.button_quit)
         button_play = findViewById(R.id.button_play)
+        button_help=findViewById(R.id.button_help)
+        button_help.setOnClickListener{
+            Toast.makeText(this, "not working", Toast.LENGTH_SHORT).show()
+        }
         button_play.setOnClickListener {
             val intent = Intent(this, StartGameActivity::class.java)
             startActivity(intent)
@@ -23,5 +28,6 @@ class HomeActivity : AppCompatActivity() {
                 System.out
             }
         }
+
     }
 }
